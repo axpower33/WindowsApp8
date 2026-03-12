17089c856e75b845a8f61117a8fc9d98d38fdac1
axpower33 <95838463+axpower33@users.noreply.github.com>
2026-03-12 15:52:24 +0700

    Add files via upload

---
+++ ProvisorBaseDataDataSet3.cs
@@ -0,0 +1,8 @@
+﻿namespace WindowsFormsApp8
+{
+
+
+    partial class ProvisorBaseDataDataSet3
+    {
+    }
+}
---
+++ fractalsDataSet.cs
@@ -0,0 +1,11 @@
+﻿namespace WindowsFormsApp8
+{
+
+
+    partial class fractalsDataSet
+    {
+        partial class FRSPDataTable
+        {
+        }
+    }
+}
